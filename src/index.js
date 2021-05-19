ce2b6cd1dac5565ccba1bce1ae789522a48e72a0
AaA <67450869+Awara-Amin@users.noreply.github.com>
2021-05-19 12:15:53 +0300

    props

--- src/index.js
+++ src/index.js
@@ -12,21 +12,20 @@ const img = "https://images-na.ssl-images-amazon.com/images/I/81eB+7+CkUL.jpg";
 function BookList() {
   return (
     <section className="booklist">
+      <Book job="developer" />
+
       <Book />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
-      {/* <h1>I love you to the Moon and Back</h1> */}
-      {/* <h3>Amelia ...</h3> */}
-      <h3>{author.toUpperCase()}</h3>{" "}
-      {/* see because you are using javaScript in JSX, you can add function directly */}
-      {/* <p>{4 + 4}</p> */}
+      <h3>{author}</h3>{" "}
     </article>
   );
 };
